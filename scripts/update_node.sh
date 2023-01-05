e8e63cb1c06dad679558763e33c114fbf742ed47
simonjiao <simon@findora.org>
2023-01-05 13:10:41 +0800

    add consensus version

--- scripts/update_node.sh
+++ scripts/update_node.sh
@@ -2,20 +2,58 @@
 
 root_dir=/tmp/findora
 node=$1
+consensus=$2
+
+if [ -z "$node" ]; then
+    echo "Empty node"
+    exit 1
+fi
+
+if [ -z "$consensus" ]; then
+    consensus="one"
+fi
 
 file=${root_dir}/devnet/${node}/config/config.toml
 
-perl -pi -e 's/(timeout_propose = )".*"/$1"9s"/' $file
-perl -pi -e 's/(timeout_propose_delta = )".*"/$1"1500ms"/' $file
-perl -pi -e 's/(timeout_prevote = )".*"/$1"3s"/' $file
-perl -pi -e 's/(timeout_prevote_delta = )".*"/$1"1500ms"/' $file
-perl -pi -e 's/(timeout_precommit = )".*"/$1"3s"/' $file
-perl -pi -e 's/(timeout_precommit_delta = )".*"/$1"1500ms"/' $file
-perl -pi -e 's/(timeout_commit = )".*"/$1"3s"/' $file
+update_consensus_1() {
+    file=$1
+    perl -pi -e 's/(timeout_propose = )".*"/$1"3s"/' $file
+    perl -pi -e 's/(timeout_propose_delta = )".*"/$1"500ms"/' $file
+    perl -pi -e 's/(timeout_prevote = )".*"/$1"1s"/' $file
+    perl -pi -e 's/(timeout_prevote_delta = )".*"/$1"500ms"/' $file
+    perl -pi -e 's/(timeout_precommit = )".*"/$1"1s"/' $file
+    perl -pi -e 's/(timeout_precommit_delta = )".*"/$1"500ms"/' $file
+    perl -pi -e 's/(timeout_commit = )".*"/$1"5s"/' $file
+}
+
+update_consensus_mainnet() {
+    file=$1
+    perl -pi -e 's/(timeout_propose = )".*"/$1"8s"/' $file
+    perl -pi -e 's/(timeout_propose_delta = )".*"/$1"100ms"/' $file
+    perl -pi -e 's/(timeout_prevote = )".*"/$1"4s"/' $file
+    perl -pi -e 's/(timeout_prevote_delta = )".*"/$1"100ms"/' $file
+    perl -pi -e 's/(timeout_precommit = )".*"/$1"4s"/' $file
+    perl -pi -e 's/(timeout_precommit_delta = )".*"/$1"100ms"/' $file
+    perl -pi -e 's/(timeout_commit = )".*"/$1"15s"/' $file
+}
 
 # kill related process and wait 5 seconds
 pids=$(ps -ef|grep devnet/node0|grep -v grep|awk '{print $2}'); for pid in ${pids}; do kill -9 "$pid"; done
 sleep 5
+
+#update consensus
+case "$consensus" in
+"one")
+    update_consensus_1 "$file"
+    ;;
+"main")
+    update_consensus_mainnet "$file"
+    ;;
+*)
+    echo "Invalid consensus"
+    exit 1
+    ;;
+esac
 
 SelfAddr=$(grep 'address' ${root_dir}/devnet/"${node}"/config/priv_validator_key.json | grep -oE '[^",]{40}');
 #ENABLE_QUERY_SERVICE=true \
