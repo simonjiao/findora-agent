e174e346bad365a52011ad8fa54ddd6f9c0cc2e6
simonjiao <simon@findora.org>
2023-01-05 13:15:34 +0800

    add default consensus

--- scripts/update_node.sh
+++ scripts/update_node.sh
@@ -15,7 +15,7 @@ fi
 
 file=${root_dir}/devnet/${node}/config/config.toml
 
-update_consensus_1() {
+update_consensus_one() {
     file=$1
     perl -pi -e 's/(timeout_propose = )".*"/$1"3s"/' $file
     perl -pi -e 's/(timeout_propose_delta = )".*"/$1"500ms"/' $file
@@ -37,6 +37,17 @@ update_consensus_mainnet() {
     perl -pi -e 's/(timeout_commit = )".*"/$1"15s"/' $file
 }
 
+update_consensus_default() {
+    file=$1
+    perl -pi -e 's/(timeout_propose = )".*"/$1"4s"/' $file
+    perl -pi -e 's/(timeout_propose_delta = )".*"/$1"2s"/' $file
+    perl -pi -e 's/(timeout_prevote = )".*"/$1"4s"/' $file
+    perl -pi -e 's/(timeout_prevote_delta = )".*"/$1"2s"/' $file
+    perl -pi -e 's/(timeout_precommit = )".*"/$1"4s"/' $file
+    perl -pi -e 's/(timeout_precommit_delta = )".*"/$1"2s"/' $file
+    perl -pi -e 's/(timeout_commit = )".*"/$1"15s"/' $file
+}
+
 # kill related process and wait 5 seconds
 pids=$(ps -ef|grep devnet/node0|grep -v grep|awk '{print $2}'); for pid in ${pids}; do kill -9 "$pid"; done
 sleep 5
@@ -44,10 +55,13 @@ sleep 5
 #update consensus
 case "$consensus" in
 "one")
-    update_consensus_1 "$file"
+    update_consensus_one "$file"
     ;;
 "main")
     update_consensus_mainnet "$file"
+    ;;
+"default")
+    update_consensus_default "$file"
     ;;
 *)
     echo "Invalid consensus"
