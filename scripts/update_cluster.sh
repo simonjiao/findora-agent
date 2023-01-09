1bb4a7efbce86f90f895acc98b8d7b9d3cef6be1
simonjiao <simon@findora.org>
2023-01-09 13:34:42 +0800

    add more test modes

--- scripts/update_cluster.sh
+++ scripts/update_cluster.sh
@@ -9,47 +9,78 @@ MAX_NODE=20
 mode=$1
 
 case "$mode" in
-"single")
-    nodes=$2
-    consensus=$3
-    ;;
-"seq")
-    consensus=$2
-    ;;
-"fast_half")
-    consensus=$2
-    ;;
-"fast_two_third")
-    consensus=$2
-    ;;
-"swarm_reboot")
-    consensus=$2
+"single" | "seq" | "fast_half" | "fast_two_third" |"swarm_reboot")
     ;;
 *)
+    echo "Invalid mode \"$mode\""
     exit 1
     ;;
 esac
 
+rm -f checkpoint.toml
 
+if [ "$mode" != "single" ]; then
+    for ((i=0; i<=MAX_NODE; i++)); do
+        nodes="$nodes node$i";
+    done
+    interval=$2
+    if [ -z "$interval" ]; then
+        interval=30
+    fi
+fi
+
+consensus=$3
 if [ -z "$consensus" ]; then
     consensus="one"
 fi
 
-if [ "$mode" != "single" ]; then
-    for ((i=0; i<MAX_NODE; i++)); do
-        nodes="$nodes node$i";
-    done
-fi
-
-if [ -z "$nodes" ]; then
-    echo "Empty node"
-    exit 1
-fi
-
 . ./update_node_env.sh
 
-for node in $nodes; do
+if [ "$mode" == "fast_half" ]; then
+    for ((i=0; i<21; i++)); do
+        if (( i > 11 )); then
+            sleep 1200
+        else
+            sleep "$interval"
+        fi
+        node="node$i"
+        file="$root_dir/devnet/$node/config/config.toml"
+        update_consensus "$consensus" "$file"
+        restart_node "$node" 5
+    done
+elif [ "$mode" == "fast_two_third" ]; then
+    for ((i=0; i<21; i++)); do
+        if (( i > 14 )); then
+            sleep 1200
+        else
+            sleep "$interval"
+        fi
+        file="$root_dir/devnet/$node/config/config.toml"
+        update_consensus "$consensus" "$file"
+        restart_node "$node" 5
+    done
+elif [ "$mode" == "swarm_reboot" ]; then
+    for node in $nodes; do
+        file="$root_dir/devnet/$node/config/config.toml"
+        update_consensus "$consensus" "$file"
+    done
+
+    for node in $nodes; do
+        restart_node "$node" 5 &
+        sleep 5
+    done
+elif [ "$mode" == "seq" ];  then
+    for node in $nodes; do
+        file="$root_dir/devnet/$node/config/config.toml"
+        update_consensus "$consensus" "$file"
+        restart_node "$node" 5
+        sleep "$interval"
+    done
+elif [ "$mode" == "single" ]; then
+    node=$2
     file="$root_dir/devnet/$node/config/config.toml"
-    update_node_env "$consensus" "$file"
+    update_consensus "$consensus" "$file"
     restart_node "$node" 5
-done
+else
+    echo "mode \"$mode\" not support yet!"
+fi
