d69daf09909189bb1971c15728337381d521d847
simonjiao <simon@findora.org>
2023-02-07 07:21:54 +0800

    update batch.sh

--- scripts/batch.sh
+++ scripts/batch.sh
@@ -1,3 +1,12 @@
+echo "fast_half_1" >> update.log
+for ((i=1; i<=10; i+=1)); do
+	cd ~/findora/platform && make debug_env; stt init; cd - || exit
+	bash -x ./update_cluster.sh fast_half_1 >>update.log 2>&1
+	cd ~/findora/platform && make stop_debug_env
+	mv /tmp/findora/devnet /tmp/findora/devnet.fast_half_1."$i"
+done
+
+<<'COMMENT'
 echo "fast_half" >> update.log
 for ((i=1; i<=10; i+=1)); do
 	cd ~/platform && make debug_env; stt init; cd - || exit
@@ -25,3 +34,4 @@ for ((i=60; i<=1200; i+=30)); do
 	bash -x ./update_cluster.sh seq "$i" >>update.log 2>&1
 	mv /tmp/findora/devnet /data/findora/devnet.seq."$i"
 done
+COMMENT
